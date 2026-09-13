9670d93900c1172233fed3411da8cabcfeaba089
michaelsdcovington <mdcovingt@gmail.com>
2026-09-13 10:48:38 -0500

    Added looping

--- calculator.c
+++ calculator.c
@@ -5,32 +5,44 @@ int main(void){
     double num2;
     double total;
     char operand;
+    char again;
 
-    printf("Enter the first number: ");
-    scanf("%lf", &num1);
-    printf("Enter the second number: ");
-    scanf("%lf", &num2);
-    printf("Enter the operand(+, -, *, /): ");
-    scanf(" %c", &operand);
+    while(again != 'n'){
+        printf("Enter the first number: ");
+        scanf("%lf", &num1);
+        printf("Enter the second number: ");
+        scanf("%lf", &num2);
+        printf("Enter the operand(+, -, *, /): ");
+        scanf(" %c", &operand);
+        switch(operand){
+            case '+':
+            total = num1 + num2;
+            break;
 
-    switch(operand){
-        case '+':
-        total = num1 + num2;
-        break;
+            case '-':
+            total = num1 - num2;
+            break;
 
-        case '-':
-        total = num1 - num2;
-        break;
+            case '*':
+            total = num1 * num2;
+            break;
 
-        case '*':
-        total = num1 * num2;
-        break;
+            case '/':
+            if(num2 == 0){
+                printf("Error: Divide by Zero");
+                return 1;
+            }
+            total = num1 / num2;
+            break;
 
-        case '/':
-        total = num1 / num2;
-        break;
+            default:
+            printf("Invalid operand. ");
+            break;
+            return 1;
+        }
+        printf("Total is: %lf\n", total);
+        printf("Calculate again? (y/n): ");
+        scanf(" %c", &again);
     }
-    printf("Total is: %lf\n", total);
-
     return 0;
 }
