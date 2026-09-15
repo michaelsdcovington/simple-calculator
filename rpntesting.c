c744e8ffc29a4898ab17551c0bf4348ed241e3aa
michaelsdcovington <mdcovingt@gmail.com>
2026-09-15 10:44:28 -0500

    Integrated calculator funtion from the first calculator

--- rpntesting.c
+++ rpntesting.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double Calculate(double num1, double num2, char operand, int *success){
+    double total = 0;
+    *success = 1;
+    switch(operand){
+        case '+':
+        total = num1 + num2;
+        break;
+
+        case '-':
+        total = num1 - num2;
+        break;
+
+        case '*':
+        total = num1 * num2;
+        break;
+
+        case '/':
+        if(num2 == 0){
+            printf("Error: Divide by Zero\n");
+            *success = 0;
+            break;
+        }
+
+        total = num1 / num2;
+        break;
+
+        default:
+        printf("Invalid operand. ");
+        *success = 0;
+    }
+    return total;
+}
 
 void Push(double stack[], int *top, double num){
     stack[*top] = num;
@@ -26,6 +58,8 @@ int main(){
         else if((token[0] == '+') || (token[0] == '-') || (token[0] == '*') || (token[0] == '/')){
             double b = Pop(stack, &top);
             double a = Pop(stack, &top);
+            result = Calculate(a, b, token[0], &isGood);
+            Push(stack, &top, result);
         }
         else{
             double num = atof(token);
